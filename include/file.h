0bfe0e5d89aff9f1f96336e47664b3c817a9155b
franpelirrojo <franfc.00@gmail.com>
2026-03-18 22:48:52 +0100

    tabla kv

--- include/file.h
+++
@@ -1,7 +0,0 @@
-#ifndef FILE_H
-#define FILE_H
-
-int create_db_file(char *filepath);
-int open_db_file(char *filepath);
-
-#endif
--- src/main.c
+++ src/main.c
@@ -1,43 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <stdbool.h>
+#include <kv.h>
 
-#include "file.h"
-
-void print_usage(char *argv[]) {
-  printf("Uso: %s -n -f <database file>\n", argv[0]);
-  printf("\t -n crea una nueva base de datos\n");
-  printf("\t -f carga una base de datos, necesita el path\n");
-  return;
-}
-
-int main(int argc, char *argv[]) {
-  int dbfd;
-  char *filename;
-  bool newfile = false;
-
-  int c;
-  opterr = 0;
-  while ((c = getopt(argc, argv, "nf:")) != -1){
-    switch (c) {
-      case 'n': 
-        newfile = true;
-        break;
-      case 'f': 
-        filename = optarg;
-        break;
-      case '?': 
-        //TODO: manejar errores concretos
-        print_usage(argv);
-        return -1;
-      default:
-        return -1;
-    }
-  }
-
-  if (newfile) {
-    dbfd = create_db_file(filename);
-  }
-
-  return 0;
+int main(int argc, char *argv[])
+{
+    kv_t *table = kv_init(5);
+    printf("%p\n", table);
+    printf("%ld\n", table->size);
+    printf("%p\n", table->entries);
+    return EXIT_SUCCESS;
 }
