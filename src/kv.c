1d0b5629173c2bd32bc909eb78ddeabf2e7e9fcc
franpelirrojo <franfc.00@gmail.com>
2026-09-15 19:48:07 +0200

    put y free

--- src/kv.c
+++ src/kv.c
@@ -1,4 +1,21 @@
 #include <kv.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TOMBSTONE 0x1
+
+size_t hash(char *key, int capacity){
+    size_t hash = 0x1337deadF;
+
+    while(*key){
+        hash ^= *key;
+        hash = hash << 8;
+        hash += *key;
+        key++;
+    }
+
+    return hash % capacity;
+}
 
 kv_t * kv_init(size_t capacity) {
     if (capacity == 0) return NULL;
@@ -17,4 +34,45 @@ kv_t * kv_init(size_t capacity) {
     }
 
     return table;
+}
+
+int kv_put(kv_t *table, char *key, char *value){
+    if (!table || !key || !value) return -1;
+
+    size_t idx = hash(key, table->capacity);
+
+    for (int i = 0; i < table->capacity - 1; i++) {
+        size_t real_idx = (idx + i) % table->capacity;
+        kv_entry_t *entry = &table->entries[real_idx];
+
+        if (entry->key &&
+                entry->key != (void*)TOMBSTONE
+                && !strcmp(entry->key, key)) {
+            char *newval = strdup(value);
+            if (!newval) return -1;
+            entry->value = newval;
+            return real_idx;
+        }
+
+        if (!entry->key || entry->key == (void*)TOMBSTONE) {
+            char *newval = strdup(value);
+            char *newkey = strdup(key);
+            if (!newval || !newkey) {
+                free(newval);
+                free(newkey);
+                return -1;
+            }
+            entry->value = newval;
+            entry->key = newkey;
+            table->count++;
+            return real_idx;
+        }
+    }
+
+    return -2;
+}
+
+void kv_free(kv_t * table) {
+    free(table->entries);
+    free(table);
 }
--- src/main.c
+++ src/main.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 #include <kv.h>
+#include <assert.h>
 
 int main(int argc, char *argv[])
 {
-    kv_t *table = kv_init(5);
-    printf("%p\n", table);
-    printf("%ld\n", table->capacity);
-    printf("%p\n", table->entries);
+    int size = 1024;
+    kv_t *table = kv_init(size);
+
+    assert(table != NULL);
+    assert(table->capacity == size);
+    assert(table->count == 0);
+    
+    kv_put(table, "hehe", "foo");
+    kv_put(table, "hehe", "boo");
+    kv_put(table, "haha", "foo");
+    kv_put(table, "Para nada es así", "joo");
+    kv_put(table, "jaja", "poo");
+
+    for (int i = 0; i < table->capacity; i++){
+        if (table->entries[i].key) {
+            printf("[%d] %s: %s\n", 
+                    i,
+                    table->entries[i].key,
+                    table->entries[i].value);
+        }
+    }
+
     return EXIT_SUCCESS;
 }
